e75769585b546b3a7d04463b0a1dacf0839667a8
milesjunior4 <milesjunior4real@gmail.com>
2025-07-27 15:04:05 +0100

    Initial commit

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -5,8 +5,8 @@ export const UserContext =  createContext();
 export function UserProvider ({children}) {
     const firmName = "LOREM";
     const refLink = "LOREM";
-    const backendBaseUrl = 'https://datasite-h33s.onrender.com'
-    // const backendBaseUrl = 'http://localhost:4000'
+    // const backendBaseUrl = 'https://datasite-h33s.onrender.com'
+    const backendBaseUrl = 'http://localhost:4000'
     const [menuBarState, setMenuBarState] = useState('-120%');
     return(
         <UserContext.Provider value={{menuBarState, setMenuBarState, firmName, refLink, backendBaseUrl}}>
